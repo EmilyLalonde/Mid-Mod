4df3c1f178838fcc854cd4079757e16c5d522a88
Emily <lalonde0902@gmail.com>
2019-08-29 11:32:55 -0600

    Add slight styling to the application

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -6,8 +6,8 @@ const Card = ({id, img, name, description, price}) => {
     <article>
       <img src={img} alt='' />
       <h2>{name}</h2>
-      <h3>{price}</h3>
       <p>{description}</p>
+      <h3>${price}</h3>
       <button> Remove From History </button>
     </article>
   )
--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -10,7 +10,7 @@ describe('CardContainer', () => {
       description: 'The latest phone to come out by Apple. It has a faster processor and improved camera to take HDR images.',
       id: 1, 
       img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xs-max-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795409154',
-      key: 1,
+      key: 1 ,
       name: 'iPhone Xs',
       price: 900
       }
