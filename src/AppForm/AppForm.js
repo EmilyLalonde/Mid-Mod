9c2c7d44f99fa52af9c82e87fe545e39636f6e53
Emily <lalonde0902@gmail.com>
2019-08-29 09:34:34 -0600

    Add setup for all component files

---
+++ src/AppForm/AppForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../AppForm/AppForm.css'
+
+class AppForm extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>Hey</div>
+    )
+  }
+}
+
+export default AppForm
