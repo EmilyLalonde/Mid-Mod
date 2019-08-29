d6e3ed6836c386b7f14cd3e6e1ae381cffdc9db6
Emily <lalonde0902@gmail.com>
2019-08-29 09:23:04 -0600

    Add fetch call to App.js to retrieve info

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+     fetch('http://localhost:3001/api/v1/purchases')
+    .then(response => response.json())
+    .then(data => this.setState({ data }))
+    .catch(error => error.message)
+  }
+
+  
   render() {
+    // console.log(this.state.data)
     return (
       <div className="App">
         <header>
